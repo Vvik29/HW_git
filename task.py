44c2498df15b90c8d27112a1a7511765f76fffe3
Vvik29 <sakharchuk29@gmail.com>
2023-02-06 18:06:44 +0200

    create file

---
+++ task.py
@@ -0,0 +1,14 @@
+
+def secondFunc(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    secondFunc("Manny")
+
+
+def newFuncIgn(number):
+    print (f'$%'*number)
+
+newFuncIgn(3)
